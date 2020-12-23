060b21cf735a7582dd91a4d927348faf488e5119
Ignas Lapėnas <ignas@lapenas.dev>
2020-12-23 10:13:31 +0200

    add initial project files

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    //read configuration file toml (Should leave configurable for sysadmin)
+    //  port should be configurable
+    //  email should be configurable
+    //read file db
+    //track time (Through database and in-memory)
+    //(zabbix should only check if program exists to verify it working correctly)
+    //control through web gui
+    //send email
+    println!("Hello, world!");
+}
