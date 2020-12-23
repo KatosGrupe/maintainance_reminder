aeb7e43846ef4cc73a90bfd1514492bfda79180f
Ignas Lapėnas <ignas@lapenas.dev>
2020-12-23 10:41:45 +0200

    add toml config

---
+++ src/config.rs
@@ -0,0 +1,46 @@
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct Config {
+    server: ServerConfig,
+    email: EmailConfig,
+}
+
+#[derive(Deserialize)]
+pub struct ServerConfig {
+    port: i32,
+    listen: String,
+}
+
+#[derive(Deserialize)]
+pub struct EmailConfig {
+    username: String,
+    password: String,
+}
+
+impl Config {
+    pub fn load() -> Config {
+        use std::fs::File;
+        use std::io::Read;
+        let mut config_file =
+            File::open("config.toml").expect("Error opening configuration file. (config.toml)");
+        let mut config: String = String::new();
+        config_file
+            .read_to_string(&mut config)
+            .expect("Error reading configuration file. (config.toml)");
+        toml::from_str(&config).expect("Failed to parse configuration file")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_works() {
+        let config = Config::load();
+        assert_eq!(config.server.port, 21904);
+        assert_eq!(config.server.listen, "127.0.0.1");
+        assert_eq!(config.email.username, "test@test.org");
+        assert_eq!(config.email.password, "SecurePassword1");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+mod config;
+
+use config::Config;
+
 fn main() {
     //read configuration file toml (Should leave configurable for sysadmin)
     //  port should be configurable
     //  email should be configurable
+    let config = Config::load();
     //read file db
     //track time (Through database and in-memory)
     //(zabbix should only check if program exists to verify it working correctly)
