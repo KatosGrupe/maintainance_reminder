eb7579a204781014edfdc2dcdcdea18f5713d29c
Ignas Lapėnas <ignas@lapenas.dev>
2021-01-07 16:15:29 +0200

    add inspection structure

---
+++ src/inspection.rs
@@ -0,0 +1,93 @@
+use chrono::prelude::*;
+use chrono::Date;
+use std::str::FromStr;
+
+#[derive(Debug)]
+pub struct InspectionDate {
+    pub id: i64,
+    pub repetition: Repetition,
+    pub date: DateTime<FixedOffset>,
+}
+
+impl InspectionDate {
+    pub fn next_time() -> Date<Utc> {
+        todo!()
+    }
+}
+
+#[derive(Debug)]
+pub enum NextTimePivotPoint {
+    Completion,
+    DueDate,
+}
+
+#[derive(Debug)]
+pub struct Repetition {
+    next_time_pivot_point: NextTimePivotPoint,
+    seconds: i32,
+}
+
+#[derive(Debug)]
+pub enum ParseError {
+    IncorrectRepetitionPivotChoice,
+    IncorrectTimeCount,
+    IncorrectTimeMultiplier,
+    RegexNotMatched,
+}
+
+impl Repetition {
+    pub fn from_string(value: &str) -> Result<Repetition, ParseError> {
+        use regex::Regex;
+        let re = Regex::new("([+]+)([0-9]+)([D|M|Y])").unwrap();
+
+        match re.captures(value) {
+            Some(cap) => {
+                let mut count: i32 = match FromStr::from_str(&cap[2]) {
+                    Ok(result) => result,
+                    Err(_) => {
+                        error!("({}) could not be converted into number, the program might not track correctly",
+                               &cap[2]);
+                        0
+                    }
+                };
+                let repetition = Repetition {
+                    next_time_pivot_point: match &cap[1] {
+                        "+" => NextTimePivotPoint::Completion,
+                        "++" => NextTimePivotPoint::DueDate,
+                        _ => {
+                            error!(
+                                "({}) could not be parsed, program might not track correctly",
+                                &cap[1]
+                            );
+                            return Err(ParseError::IncorrectRepetitionPivotChoice);
+                        }
+                    },
+                    seconds: match &cap[3] {
+                        "Y" => count * 60 * 60 * 24 * 365,
+                        "M" => count * 60 * 60 * 24 * 30,
+                        "W" => count * 60 * 60 * 24 * 7,
+                        "D" => count * 60 * 60 * 24,
+                        "h" => count * 60 * 60,
+                        "m" => count * 60,
+                        "s" => count,
+                        other => {
+                            error!(
+                                "({}) could not be parsed, acceptable values (Y, M, D, W, h, m, s)",
+                                &cap[3]
+                            );
+                            count
+                        }
+                    },
+                };
+                return Ok(repetition);
+            }
+            None => {
+                error!(
+                    "({}) could not be parsed, program might not track correctly",
+                    value
+                );
+                return Err(ParseError::RegexNotMatched);
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,63 @@
-mod config;
+#[macro_use]
+extern crate log;
+extern crate simplelog;
 
+mod config;
+mod inspection;
+
+use chrono::prelude::*;
 use config::Config;
+use inspection::InspectionDate;
+use inspection::Repetition;
+use simplelog::*;
 
 fn main() {
+    CombinedLogger::init(vec![TermLogger::new(
+        LevelFilter::Info,
+        simplelog::Config::default(),
+        TerminalMode::Mixed,
+    )])
+    .unwrap();
     //read configuration file toml (Should leave configurable for sysadmin)
     //  port should be configurable
     //  email should be configurable
     let config = Config::load();
     //read file db
-    //track time (Through database and in-memory)
+    let connection =
+        sqlite::open("data.db").expect("Failed to load inspections database (data.db)");
+
+    let mut cursor = connection
+        .prepare(
+            "
+select i.id,
+       i.repetition,
+       coalesce(max(id.completed_on), '0000-01-01T00:00:00+00:00') as last_completion_time from inspections i
+left join inspection_date id on i.id = id.inspection_id
+group by i.id, i.repetition;
+",
+        )
+        .expect("Failed to prepare query")
+        .cursor();
+    let mut inspections = vec![];
+
+    while let Some(row) = cursor.next().unwrap() {
+        inspections.push(InspectionDate {
+            id: row[0].as_integer().unwrap(),
+            repetition: Repetition::from_string(row[1].as_string().unwrap())
+                .expect("Failed to parse"),
+            date: match DateTime::parse_from_rfc3339(row[2].as_string().unwrap()) {
+                Ok(value) => value,
+                Err(error) => panic!(
+                    "Failed to parse ({}): {}",
+                    error,
+                    row[2].as_string().unwrap()
+                ),
+            },
+        })
+    }
+
+    println!("{:#?}", inspections);
+
     //(zabbix should only check if program exists to verify it working correctly)
     //control through web gui
     //send email
